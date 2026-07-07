109d5df75c8f9c09237fa131080aad8f1193f471
jeio09caballes-a11y <jeio09caballes@gmail.com>
2026-07-07 22:36:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("Happy Birthday, Mickey ❤️");
+const startBtn = document.getElementById("startBtn");
+const openBook = document.getElementById("openBook");
+
+const loading = document.getElementById("loading");
+const scrapbook = document.getElementById("scrapbook");
+
+startBtn.onclick = () => {
+    loading.style.display = "none";
+    scrapbook.classList.remove("hidden");
+};
+
+openBook.onclick = () => {
+    alert("Next page coming soon... ❤️");
+};
